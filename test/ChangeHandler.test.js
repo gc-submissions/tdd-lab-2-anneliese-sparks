854f054bcde10cdf4cf4440ad002b25d40a31a07
Anneliese Sparks <sparksanneliese@gmail.com>
2021-04-28 16:20:21 -0400

    constructor tests added

--- test/ChangeHandler.test.js
+++ test/ChangeHandler.test.js
@@ -8,5 +8,16 @@ describe("ChangeHandler", function() {
 
   // TODO: Add additional tests below...
 
+  test("amountDue is set based on argument",() => {
+    // Remember, you can arrange, act, and assert...start small
+    const changeHandler = new ChangeHandler();
+    expect(changeHandler.amountDue).toEqual(null);
+  })
+
+  test("cashTendered is set to zero", () => {
+    // Remember, you can arrange, act, and assert...start small
+    const changeHandler = new ChangeHandler();
+    expect(changeHandler.cashTendered).toEqual(0);
+  })
 
 });
