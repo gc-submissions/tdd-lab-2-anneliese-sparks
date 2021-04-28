328691360bb981f5ddb1b2c88d0f833982461f73
Anneliese Sparks <sparksanneliese@gmail.com>
2021-04-28 16:50:01 -0400

    insertCoin tests complete

--- test/ChangeHandler.test.js
+++ test/ChangeHandler.test.js
@@ -20,4 +20,28 @@ describe("ChangeHandler", function() {
     expect(changeHandler.cashTendered).toEqual(0);
   })
 
+  test("inserting a quarter adds 25 cents", () => {
+    const changeHandler = new ChangeHandler();
+    changeHandler.insertCoin("quarter");
+    expect(changeHandler.cashTendered).toEqual(25); 
+  })
+
+  test("inserting a dime adds 10 cents", () => {
+    const changeHandler = new ChangeHandler();
+    changeHandler.insertCoin("dime");
+    expect(changeHandler.cashTendered).toEqual(10); 
+  })
+
+  test("inserting a nickel adds 5 cents", () => {
+    const changeHandler = new ChangeHandler();
+    changeHandler.insertCoin("nickel");
+    expect(changeHandler.cashTendered).toEqual(5); 
+  })
+
+  test("inserting a penny adds 1 cent", () => {
+    const changeHandler = new ChangeHandler();
+    changeHandler.insertCoin("penny");
+    expect(changeHandler.cashTendered).toEqual(1); 
+  })
+
 });
